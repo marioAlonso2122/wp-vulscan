0ee2afa806f4404390a6d4c23118231a696b8aa1
Mario Alonso <marioalonsopulgar@gmail.com>
2025-05-28 16:44:53 +0200

    Inicio del desarrollo de WP-VulScan

---
+++ plugin/wp-vulscan.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: WP-VulScan
+Description: Plugin para la detección de vulnerabilidades en instalaciones WordPress.
+Version: 1.0
+Author: Mario Alonso Pulgar
+License: GPL2
+*/
